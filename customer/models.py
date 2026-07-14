8d69a5b61fb27b1b1c52081b5e609b619b893ca7
ABOLFAZL <abolfazlmirzaie.1385@gmail.com>
2026-07-15 00:03:13 +0330

    change customer model

--- customer/models.py
+++ customer/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 
 class Customer(models.Model):
+    telegram_id = models.BigIntegerField(unique=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(blank=True, null=True)
     phone = models.CharField(max_length=11)
-    address = models.TextField()
+    address = models.TextField(blank=True, null=True)
     status = models.CharField(
         max_length=100,
         choices=[
